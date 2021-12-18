4484b8573f39b9e5ade3c43459029cd94845ebab
Adolfo Castelo <adolfo.castelo@fing.edu.uy>
2021-12-18 18:32:13 -0300

    encriptation done

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,6 +1,8 @@
-function hash(password){
-    return '-1-1mdls';
-}
+const crypto = require('crypto');
+const config = require('../config');
 
-
-module.exports = hash;
+module.exports = function (data) {
+    const sha256gen = crypto.createHmac('sha256', config.passwordSalt);
+    const result = sha256gen.update(data).digest('hex');
+    return result;
+};
