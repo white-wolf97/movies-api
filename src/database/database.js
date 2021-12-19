c2045dd8a73522b11a21c0badb1d172ec88eac50
adolfoCastelo1 <adolfocastelo99@gmail.com>
2021-12-19 13:14:18 -0300

    database initialization done

---
+++ src/database/database.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const DatabaseError = require('../exceptions/databaseError.js');
+
+module.exports = class Database{
+
+    static init(){ 
+        if(!fs.existsSync(path.join(__dirname, '..', 'database', 'users.txt')))
+            fs.writeFile(path.join(__dirname, '..', 'database', 'users.txt'),'', (err) => {
+                if(err)
+                    throw new DatabaseError(err.message);
+            });
+        
+        if(!fs.existsSync(path.join(__dirname, '..', 'database', 'favorites.txt')))
+            fs.writeFile(path.join(__dirname, '..', 'database', 'favorites.txt'),'', (err) => {
+                if(err)
+                    throw new DatabaseError(err.message);
+            });
+ 
+        if(!fs.existsSync(path.join(__dirname, '..', 'database', 'tokenBlacklist.txt')))
+            fs.writeFile(path.join(__dirname, '..', 'database', 'tokenBlacklist.txt'),'', (err) => {
+                if(err)    
+                    throw new DatabaseError(err.message);
+            });
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const config = require('./config');
+const Database = require('./database/database.js')
 const fs = require('fs');
 const app = express();
 const {movieRouter, authRouter, userRouter} = require('./routes/apiv1.js');
-
+ 
+try{ 
+    Database.init();
+}   
+catch(err){  
+    console.log('Error initializing database');
+    console.log(err);
+    console.log(err.stack);
+}
+ 
 app.use(express.json());//To be able to parse the req body parameters. 
 app.use('/api/v1/movies', movieRouter);
 app.use('/api/v1/auth', authRouter);
